3cd3bee4ac5a96811477a0e818094ec0d712b201
valentasklimas <valentasklimas@gmail.com>
2023-01-03 22:45:06 +0000

    Add Dapper

---
+++ The_Super_App/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Super_App.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? CellPhoneId { get; set; }
+    }
+}
--- The_Super_App/Program.cs
+++ The_Super_App/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Dapper;
+using System.Data.SqlClient;
+using The_Super_App.Models;
+
 Console.WriteLine("Hello, The Super App");
+
+var cs = @"Server=(localdb)\MSSQLLocalDB;Database=DapperDB;Trusted_Connection=True;;";
+
+using var con = new SqlConnection(cs);
+con.Open();
+
+var version = con.Query<Person>("select * from Person").ToList();
+
+version.ForEach(person => Console.WriteLine($"{person.Id}, {person.FirstName}"));
+con.Close();
