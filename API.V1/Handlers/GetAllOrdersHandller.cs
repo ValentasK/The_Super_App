482802e6fc8e68ddcf0887a9af7fb70f44667b23
valentasklimas <valentasklimas@gmail.com>
2023-01-11 20:11:52 +0000

    Added connection string

--- API.V1/Handlers/GetAllOrdersHandller.cs
+++ API.V1/Handlers/GetAllOrdersHandller.cs
@@ -2,25 +2,31 @@
 using API.V1.Responses;
 using MediatR;
 using Microsoft.Data.SqlClient;
-using System;
 using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 namespace API.V1.Handlers
 {
     public class GetAllCustomersHandller : IRequestHandler<GetAllCustomersQuery, List<CustomerResponse>>
     {
+         IConfiguration _configuration;
+        public GetAllCustomersHandller(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public async Task<List<CustomerResponse>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
         {
+            string? connectionString = _configuration.GetConnectionString("myDb1");
 
-            var cs = @"Server=(localdb)\MSSQLLocalDB;Database=DapperDB;Trusted_Connection=True;";
-            using var con = new SqlConnection(cs);
+            using var con = new SqlConnection(connectionString);
             con.Open();
 
             string sql = "select * from [DapperDB].[dbo].[Person] as P left join [DapperDB].[dbo].[Phone] as ph on p.CellPhoneId = ph.Id";
             var p = new DynamicParameters();
 
-            var customers = con.Query<CustomerResponse>(sql,p).ToList();
-            
+            var customers = con.Query<CustomerResponse>(sql, p).ToList();
+
             return customers;
         }
     }
